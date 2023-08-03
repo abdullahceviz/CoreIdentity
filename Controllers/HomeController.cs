fa61fcafd3e8b5dc51c24746c52a2a8ae7b849b3
Abdullah Ceviz <abdullah.ceviz.resmi@gmail.com>
2023-08-03 12:27:15 +0300

    SingUp

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,4 +1,6 @@
 ﻿using AspNetCoreIdentityApp.Web.Models;
+using AspNetCoreIdentityApp.Web.Models.ViewModels;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
@@ -7,10 +9,12 @@ namespace AspNetCoreIdentityApp.Web.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly UserManager<AppUser> _userManager;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger,UserManager<AppUser> userManager)
         {
             _logger = logger;
+            _userManager = userManager;
         }
 
         public IActionResult Index()
@@ -27,6 +31,35 @@ namespace AspNetCoreIdentityApp.Web.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> SignUp(SignUpViewModel request)
+        {
+            if(!ModelState.IsValid)
+            {
+                return View();
+            }
+            var identityresult = await _userManager.CreateAsync(new()
+            {
+                UserName = request.UserName,
+                Email = request.Email,
+                PhoneNumber = request.Phone
+
+            }, request.PasswordConfirm);
+            if (identityresult.Succeeded)
+            {
+                TempData["SuccessMessage"] = "Üyelik kayıt işlemi başarıtla gerçekleşmiştir.";
+                return RedirectToAction(nameof(HomeController.SignUp));
+            }
+            foreach(IdentityError item in identityresult.Errors)
+            {
+                ModelState.AddModelError(string.Empty,item.Description);
+            }
+            return View();
         }
     }
 }
---
+++ Models/ViewModels/SignUpViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AspNetCoreIdentityApp.Web.Models.ViewModels
+{
+    public class SignUpViewModel
+    {
+        public SignUpViewModel()
+        {
+            
+        }
+        public SignUpViewModel(string userName, string email, string phone, string password)
+        {
+            UserName = userName;
+            Email = email;
+            Phone = phone;
+            Password = password;
+        }
+        [Required(ErrorMessage ="Kullancı Ad alanı boş bırakılamaz.")]
+        [Display(Name="Kullanıcı Adi :")]
+        public string UserName { get; set; }
+        [Display(Name = "Email :")]
+        [Required(ErrorMessage = "Email alanı boş bırakılamaz.")]
+        [EmailAddress(ErrorMessage ="Email formatı yanlıştır.")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Telefon alanı boş bırakılamaz.")]
+        [Display(Name = "Telefon :")]
+        public string Phone { get; set; }
+        [Required(ErrorMessage = "Şifre alanı boş bırakılamaz.")]
+        [Display(Name = "Şifre :")]
+        public string Password { get; set; }
+        [Required(ErrorMessage = "Şifre Tekrar alanı boş bırakılamaz.")]
+        [Display(Name = "Şifre Tekrar :")]
+        [Compare(nameof(Password),ErrorMessage ="Şifre aynı değildir.")]
+        public string PasswordConfirm { get; set; }
+    }
+}
