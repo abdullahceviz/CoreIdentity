0bd12dfb9df1fddda0aa23ef8538b97f543f5a66
Abdullah Ceviz <abdullah.ceviz.resmi@gmail.com>
2023-08-03 09:01:57 +0300

    efcore

--- Program.cs
+++ Program.cs
@@ -1,8 +1,15 @@
+using AspNetCoreIdentityApp.Web.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("SqlCon"));
+});
+builder.Services.AddIdentity<AppUser, AppRole>().AddEntityFrameworkStores<AppDbContext>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
