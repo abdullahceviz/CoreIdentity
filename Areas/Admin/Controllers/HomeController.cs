8f5f0c192446ed5884dbbfca56fdcf273d35cf4c
Abdullah Ceviz <abdullah.ceviz.resmi@gmail.com>
2023-08-04 23:34:13 +0300

    Admin area

---
+++ Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCoreIdentityApp.Web.Areas.Admin.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult UserList()
+        {
+            return View();
+        }
+    }
+}
