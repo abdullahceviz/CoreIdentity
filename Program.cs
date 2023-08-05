b56ae15d426b38954a12bcb8647a8090a08a0fb5
Abdullah Ceviz <abdullah.ceviz.resmi@gmail.com>
2023-08-05 14:58:02 +0300

    Cookie options

--- Program.cs
+++ Program.cs
@@ -11,6 +11,18 @@ builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("SqlCon"));
 });
 builder.Services.AddIdentityWithExtensions();
+builder.Services.ConfigureApplicationCookie(options =>
+{
+    var cookieBuilder = new CookieBuilder();
+    cookieBuilder.Name = "UdemyAppCookie";
+    options.LoginPath = new PathString("/Home/Signin");
+    options.LogoutPath = new PathString("/Member/Logout");
+    options.Cookie = cookieBuilder;
+    //60 gün cookie tut
+    options.ExpireTimeSpan = TimeSpan.FromDays(60);
+    //cookie yenile
+    options.SlidingExpiration = true;
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -25,7 +37,7 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
-
+app.UseAuthentication();
 app.UseAuthorization();
 app.MapControllerRoute(
     name: "areas",
