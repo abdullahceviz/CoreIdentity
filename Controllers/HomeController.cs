b00b6bd1fd642faa5d6bbf3b972a25520a0c9124
Abdullah Ceviz <abdullah.ceviz.resmi@gmail.com>
2023-08-05 12:44:11 +0300

    SignIn

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using AspNetCoreIdentityApp.Web.Models.ViewModels;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using AspNetCoreIdentityApp.Web.Extensions;
 
 namespace AspNetCoreIdentityApp.Web.Controllers
 {
@@ -10,11 +11,13 @@ namespace AspNetCoreIdentityApp.Web.Controllers
     {
         private readonly ILogger<HomeController> _logger;
         private readonly UserManager<AppUser> _userManager;
+        private readonly SignInManager<AppUser> _signInManager;
 
-        public HomeController(ILogger<HomeController> logger,UserManager<AppUser> userManager)
+        public HomeController(ILogger<HomeController> logger, UserManager<AppUser> userManager, SignInManager<AppUser> signInManager)
         {
             _logger = logger;
             _userManager = userManager;
+            _signInManager = signInManager;
         }
 
         public IActionResult Index()
@@ -32,6 +35,28 @@ namespace AspNetCoreIdentityApp.Web.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+        public IActionResult SignIn()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> SignIn(SignInViewModel model, string returnUrl = null)
+        {
+            returnUrl = returnUrl ?? Url.Action("Index", "Home");
+            var hasUser = await _userManager.FindByEmailAsync(model.Email);
+            if (hasUser == null)
+            {
+                ModelState.AddModelError(string.Empty, "Email veya şifre yanlış");
+                return View();
+            }
+            var signInResult = await _signInManager.PasswordSignInAsync(hasUser, model.Password, model.RememberMe, false);
+            if (signInResult.Succeeded)
+            {
+                return Redirect(returnUrl);
+            }
+            ModelState.AddModelError(string.Empty, "Email veya şifre yanlış");
+            return View();
+        }
         public IActionResult SignUp()
         {
             return View();
@@ -39,7 +64,7 @@ namespace AspNetCoreIdentityApp.Web.Controllers
         [HttpPost]
         public async Task<IActionResult> SignUp(SignUpViewModel request)
         {
-            if(!ModelState.IsValid)
+            if (!ModelState.IsValid)
             {
                 return View();
             }
@@ -55,10 +80,7 @@ namespace AspNetCoreIdentityApp.Web.Controllers
                 TempData["SuccessMessage"] = "Üyelik kayıt işlemi başarıtla gerçekleşmiştir.";
                 return RedirectToAction(nameof(HomeController.SignUp));
             }
-            foreach(IdentityError item in identityresult.Errors)
-            {
-                ModelState.AddModelError(string.Empty,item.Description);
-            }
+            ModelState.AddModelErrorList(identityresult.Errors.Select(x => x.Description).ToList());
             return View();
         }
     }
