7fe57989772c4466b1a727d3ad553cb0782b9a08
Abdullah Ceviz <abdullah.ceviz.resmi@gmail.com>
2023-08-12 18:49:45 +0300

    add roles controller

---
+++ Areas/Admin/Controllers/RolesController.cs
@@ -0,0 +1,24 @@
+﻿using AspNetCoreIdentityApp.Web.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCoreIdentityApp.Web.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class RolesController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly RoleManager<AppRole> _roleManager;
+
+        public RolesController(RoleManager<AppRole> roleManager, UserManager<AppUser> userManager)
+        {
+            _roleManager = roleManager;
+            _userManager = userManager;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
